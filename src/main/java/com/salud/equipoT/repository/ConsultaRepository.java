6375f7949572e22fb7738f25e6fb22b66fffac0d
Matias <akucfh@gmail.com>
2023-11-23 00:03:52 -0300

    agregandoRepositorys

---
+++ src/main/java/com/salud/equipoT/repository/ConsultaRepository.java
@@ -0,0 +1,9 @@
+package com.salud.equipoT.repository;
+
+import com.salud.equipoT.entidad.Consulta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
+
+}
---
+++ src/main/java/com/salud/equipoT/repository/HistoriaClinicaRepository.java
@@ -0,0 +1,8 @@
+package com.salud.equipoT.repository;
+
+import com.salud.equipoT.entidad.HistoriaClinica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface HistoriaClinicaRepository extends JpaRepository<HistoriaClinica, Long> {
+}
